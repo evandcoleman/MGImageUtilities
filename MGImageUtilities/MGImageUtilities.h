3e36ebcbbe0de0c0fb21c31977d80a217ec05a1f
Evan Coleman <e@edc.me>
2019-10-20 22:05:17 -0400

    add carthage support

---
+++ MGImageUtilities/MGImageUtilities.h
@@ -0,0 +1,18 @@
+//
+//  MGImageUtilities.h
+//  MGImageUtilities
+//
+//  Created by Evan Coleman on 10/20/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MGImageUtilities.
+FOUNDATION_EXPORT double MGImageUtilitiesVersionNumber;
+
+//! Project version string for MGImageUtilities.
+FOUNDATION_EXPORT const unsigned char MGImageUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGImageUtilities/PublicHeader.h>
+
+
